d0e84f168954a10290b7ae94ef2c50db68495ecd
asi <yuexin.shi@gmail.com>
2016-12-29 23:18:36 +0800

    运行时权限封装

--- core/src/main/java/org/asi/core/base/BaseActivity.java
+++ core/src/main/java/org/asi/core/base/BaseActivity.java
@@ -1,19 +1,29 @@
 package org.asi.core.base;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 
 public abstract class BaseActivity<V extends  BaseView,T extends BasePresenter<V>> extends AppCompatActivity {
 
     protected T mPresenter;
-
+    /**
+     * 权限授权回掉
+     */
+    PermissionListener mPermissionListener;
     protected abstract int getLayoutResource();
 
     protected abstract void onInitView(Bundle bundle);
@@ -66,5 +76,67 @@ public abstract class BaseActivity<V extends  BaseView,T extends BasePresenter<V
     protected void replaceFragment(int id, Fragment fragment) {
         FragmentManager supportFragmentManager = getSupportFragmentManager();
         supportFragmentManager.beginTransaction().replace(id,fragment).commit();
+    }
+
+
+
+    /**
+     * 权限申请
+     * @param permissions
+     * @param listener
+     */
+    protected void requestRunTimePermission(String[] permissions, PermissionListener listener) {
+
+        //todo 获取栈顶activity，如果null。return；
+
+        this.mPermissionListener = listener;
+
+        List<String> permissionList = new ArrayList<>();
+        for (String permission : permissions) {
+            if(ContextCompat.checkSelfPermission(this,permission)!= PackageManager.PERMISSION_GRANTED){
+                permissionList.add(permission);
+            }
+        }
+        if(!permissionList.isEmpty()){
+            ActivityCompat.requestPermissions(this,permissionList.toArray(new String[permissionList.size()]),1);
+        }else{
+            listener.onGranted();
+        }
+    }
+
+    /**
+     * 申请结果
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0){
+                    List<String> deniedPermissions = new ArrayList<>();
+                    List<String> grantedPermissions = new ArrayList<>();
+                    for (int i = 0; i < grantResults.length; i++) {
+                        int grantResult = grantResults[i];
+                        if (grantResult != PackageManager.PERMISSION_GRANTED){
+                            String permission = permissions[i];
+                            deniedPermissions.add(permission);
+                        }else{
+                            String permission = permissions[i];
+                            grantedPermissions.add(permission);
+                        }
+                    }
+
+                    if (deniedPermissions.isEmpty()){
+                        mPermissionListener.onGranted();
+                    }else{
+                        mPermissionListener.onDenied(deniedPermissions);
+                        mPermissionListener.onGranted(grantedPermissions);
+                    }
+                }
+                break;
+        }
     }
 }
---
+++ core/src/main/java/org/asi/core/base/PermissionListener.java
@@ -0,0 +1,16 @@
+package org.asi.core.base;
+
+import java.util.List;
+
+/**
+ * Created by asi on 2016/12/27.
+ */
+
+public  interface PermissionListener {
+    //全部授权成功
+    void onGranted();
+    //授权部分
+    void  onGranted(List<String> grantedPermission);
+    //拒绝授权
+    void  onDenied(List<String> deniedPermission);
+}
